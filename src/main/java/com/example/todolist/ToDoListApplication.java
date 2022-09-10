ace7abd4adcb451e6ebe0ad6ca2f505f37aaed10
Dejvvit13 <92039043+Dejvvit13@users.noreply.github.com>
2022-09-10 22:10:43 +0200

    Added spring dependencies and built model

--- src/main/java/com/example/todolist/ToDoListApplication.java
+++ src/main/java/com/example/todolist/ToDoListApplication.java
@@ -2,9 +2,25 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class ToDoListApplication {
+public class ToDoListApplication implements RepositoryRestConfigurer {
+
+    @Bean
+    Validator validator() {
+       return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        validatingListener.addValidator("beforeCreate", validator());
+        validatingListener.addValidator("brforeSave", validator());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoListApplication.class, args);
--- src/main/java/com/example/todolist/model/Task.java
+++ src/main/java/com/example/todolist/model/Task.java
@@ -1,15 +1,22 @@
 package com.example.todolist.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Getter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Getter
 @Entity
 @Table(name = "tasks")
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Task description can't be empty")
     private String description;
     private boolean done;
 
+    public Task() {
+        // TODO document why this constructor is empty
+    }
 }
